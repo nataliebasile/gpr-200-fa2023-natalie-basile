e5a583d2cbdf47c583f4b0655b59d478bc750379
natalie basile <natalie.basile@mymail.champlain.edu>
2023-10-24 12:47:22 -0400

    small fix tee hee

--- core/nb/camera.h
+++ core/nb/camera.h
@@ -15,7 +15,7 @@ namespace nb {
 		float orthoSize; // Height of orthographic frustum
 		// World -> View
 		inline ew::Mat4 ViewMatrix() {
-			return LookAt(position, target, ew::Vec3(0, 1, 0));
+			return nb::LookAt(position, target, ew::Vec3(0, 1, 0));
 		};
 		// View -> Clip
 		inline ew::Mat4 ProjectionMatrix() {
